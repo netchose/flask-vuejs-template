a5c4a7b6b394d59c3ad426c0052d81a8b8a75b74
netchose <alexandre.lancar@gmail.com>
2025-06-06 08:04:19 +0200

    Remove yarn 4 artifacts

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './filters'
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
